9baa3f732f3371d4a3793eb7baa0b759015e9b63
Muhammad Musa Mueed <musavuswh@gmail.com>
2026-03-14 14:10:06 +0500

    layout files added

---
+++ B2B_Teams_Project_Management_SaaS/client/src/routes/auth.route.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const AuthRoute = () => {
+  return (
+    <Outlet />
+  )
+}
+
+export default AuthRoute
--- B2B_Teams_Project_Management_SaaS/client/src/routes/index.tsx
+++ B2B_Teams_Project_Management_SaaS/client/src/routes/index.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { authenticationRoutePaths, baseRoutePaths, protectedRoutePaths } from "./common/routes"
+import AuthRoute from "./auth.route"
+import AppLayout from "@/layout/app.layout"
+import BaseLayout from "@/layout/base.layout"
 
 const AppRoutes = () => {
   return (
@@ -9,26 +12,31 @@ const AppRoutes = () => {
     {/* Routes Wrapper */}
     <Routes>
 
-      {/* base layout routes */}
-      <Route>
+      {/* no auth + base layout - routes */}
+      <Route element={<BaseLayout />}>
         {baseRoutePaths.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
         ))}
       </Route>
 
-      {/* authentication + base layout routes */}
-      <Route>
-        {authenticationRoutePaths.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
+      {/* authentication + base layout - routes */}
+      <Route element={<AuthRoute />}>
+        <Route element={<BaseLayout />}>
+          {authenticationRoutePaths.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Route>
       </Route>
 
-      {/* protected + app layout routes */}
-      <Route>
+      {/* protected + app layout - routes */}
+      <Route element={<AppLayout />}>
         {protectedRoutePaths.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
         ))}
       </Route>
+
+      {/* Catch all undefined routes */}
+      <Route path="*" element={"Not Found"} />
     </Routes>
     </BrowserRouter>
   )
