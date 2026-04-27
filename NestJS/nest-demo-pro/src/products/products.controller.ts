6b093d3a5ab44e555fe0f142600233f9c28eb70a
Muhammad Musa Mueed <musavuswh@gmail.com>
2026-04-28 01:55:15 +0500

    feat: refactor product module and controller structure for improved organization

--- NestJS/nest-demo-pro/src/products/products.controller.ts
+++ NestJS/nest-demo-pro/src/products/products.controller.ts
@@ -3,46 +3,52 @@ import type { AllProductsResponse } from './types/types';
 
 @Controller('products')
 export class ProductsController {
+    /**
+     * 
     @Get('/')
     @HttpCode(301)
     @Header('Authorization', `Bearer ${process.env.API_KEY}`)
     findAll(
-        @Query() query
-    ): AllProductsResponse {
-        // Logger.log(request.ip);
-        if (query.limit) Logger.log(query.limit);
-        return {
-            success: true,
-            data: {
-                products: [
-                    'iPhone 14 Pro Max',
-                    'iPhone 14 Pro',
-                    'iPhone 14',
-                    'iPhone 14 Plus'
-                ]
-            },
-            total: 4,
-            message: 'Products retrieved successfully'
-        }
-    }
+     @Query() query
+ ): AllProductsResponse {
+     // Logger.log(request.ip);
+     if (query.limit) Logger.log(query.limit);
+     return {
+         success: true,
+         data: {
+             products: [
+                 'iPhone 14 Pro Max',
+                 'iPhone 14 Pro',
+                 'iPhone 14',
+                 'iPhone 14 Plus'
+             ]
+         },
+         total: 4,
+         message: 'Products retrieved successfully'
+     }
+ }
+ 
+ @Get('/docs')
+ @Redirect('https://docs.nestjs.com', 302)
+ getDocs(
+     @Query('version') version
+ ) {
+     if (version && version === 5)
+     return {
+     url: 'https://docs.nestjs.com/v5'
+ }
+ }
+ 
+ @Get(':id')
+ findOne<T>(
+     @Param('id') id: T
+ ): {id: T} {
+     return {
+         id
+     }
+ }
+ */
 
-    @Get('/docs')
-    @Redirect('https://docs.nestjs.com', 302)
-    getDocs(
-        @Query('version') version
-    ) {
-        if (version && version === 5)
-            return {
-        url: 'https://docs.nestjs.com/v5'
-            }
-    }
 
-    @Get(':id')
-    findOne<T>(
-        @Param('id') id: T
-    ): {id: T} {
-        return {
-            id
-        }
-    }
+
 }
